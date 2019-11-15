f8b92e3b4cdd81c4a560ac63e202b0b367ad227b
niklss <albert_badretdin@mail.ru>
2019-11-15 19:41:55 +0300

    Main commit

--- src/main/scala/app.scala
+++ src/main/scala/app.scala
@@ -1,19 +1,34 @@
 import org.apache.spark._
 import org.apache.spark.streaming._
-import org.apache.spark.streaming.StreamingContext._ // not necessary since Spark 1.3
 
 
-val conf = new SparkConf().setMaster("local[2]").setAppName("NetworkWordCount")
-val ssc = new StreamingContext(conf, Seconds(1))
+object Main {
+  def main(args: Array[String]) {
+    val conf = new SparkConf().setMaster("local[2]").setAppName("NetworkWordCount")
+    val ssc = new StreamingContext(conf, Seconds(1))
 
-val lines = ssc.socketTextStream("10.91.66.168", 8989)
+    val lines = ssc.socketTextStream("10.91.66.168", 8989)
+    print(lines)
+//    lines.saveAsTextFiles("")
 
-val words = lines.flatMap(_.split(" "))
 
-val pairs = words.map(word => (word, 1))
-val wordCounts = pairs.reduceByKey(_ + _)
 
-wordCounts.print()
+//    lines.map(status => status.getText).map(tweet => (tweet, sentiment(tweet)))
+//      .foreachRDD(rdd => rdd.collect().foreach(tuple => println(" Sentiment => " + tuple._2 + " :-: TWEET => " + tuple._1)))
 
-ssc.start()             // Start the computation
-ssc.awaitTermination() 
+    val words = lines.flatMap(_.split(" "))
+
+
+    val pairs = words.map(word => (word, 1))
+    val wordCounts = pairs.reduceByKey(_ + _)
+
+    wordCounts.print()
+
+    ssc.start() // Start the computation
+    ssc.awaitTermination()
+  }
+
+  def sentiment(value: Any): Unit ={
+
+  }
+}
