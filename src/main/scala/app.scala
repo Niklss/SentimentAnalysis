0a33759d3d47fae98d9eb53dc38237e0916f5e62
SimonWT <y.yudinskikh@innopolis.ru>
2019-11-19 21:24:44 +0300

    before stash pop

--- src/main/scala/app.scala
+++ src/main/scala/app.scala
@@ -1,20 +1,47 @@
 import org.apache.spark._
+import org.apache.spark.sql._
+import org.apache.spark.sql.functions
+import org.apache.spark.sql.functions.{lit}
 import org.apache.spark.streaming._
-
+import java.time.{Instant, ZoneId, ZonedDateTime}
 
 object Main {
   def main(args: Array[String]) {
-    val conf = new SparkConf().setMaster("local[2]").setAppName("NetworkWordCount")
-    val ssc = new StreamingContext(conf, Seconds(200))
+    val conf = new SparkConf().setMaster("local[2]").setAppName("Twitter-Sentiment")
+    val ssc = new StreamingContext(conf, Seconds(20))
+    val spark = SparkSession
+      .builder
+      .appName("SentimentClassifier")
+      .config("spark.master", "local")
+      .getOrCreate()
+    spark.sparkContext.setLogLevel("ERROR")
+    import spark.implicits._
 
     val lines = ssc.socketTextStream("10.91.66.168", 8989)
 //    val lines = ssc.socketTextStream("localhost", 9999)
     val words = lines.flatMap(_.split(" "))
     val count = lines.count()
+    lines.foreachRDD(
+      (rdd, time) =>
+        if (!rdd.isEmpty()) {
+          val df = rdd.toDF
+          // do something
+          df.collect.foreach(println)
+          println(time)
+          val df_out = df.withColumn("time", lit(time.toString())). //Time when message arrived
+            withColumn("class", lit(1.toString)) //Write the class of this message
+
+          df_out.repartition(1)
+              .write.format("com.databricks.spark.csv")
+              .mode("append")
+              .save("output.csv")
+
+        }
+    )
 //    val wordCounts = words.map(x => (x, 1)).reduceByKey(_ + _)
-    lines.print()
-    count.print()
-    words.print()
+//    lines.print()
+//    count.print()
+//    words.print()
 
     print("GOVNO_ZALUPAAAAAAAAAAAAAAA")
 
